48f07161c31a3e6914f882a1f9206260d8a70b88
Rahmat Trinanda <rahmat3nanda@gmail.com>
2025-02-13 18:10:19 +0700

    Initial Commit

---
+++ CookIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookIt
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 13/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
