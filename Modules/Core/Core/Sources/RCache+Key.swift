affb4149586fbaee0887b41f4446cfb173b52412
Rahmat Trinanda <rahmat3nanda@gmail.com>
2025-02-14 16:42:34 +0700

    Create RCacheKey

---
+++ Modules/Core/Core/Sources/RCache+Key.swift
@@ -0,0 +1,12 @@
+//
+//  RCache+Key.swift
+//  Core
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 14/02/25.
+//
+
+import RCache
+
+extension RCache.Key {
+   static let isFirstLaunch = RCache.Key("isFirstLaunch")
+}
