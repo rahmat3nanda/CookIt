5855ad9bb047d8fb6eda2eaf8b0f0f36e7e34d05
Rahmat Trinanda <rahmat3nanda@gmail.com>
2025-02-15 13:19:58 +0700

    Create Gacha func

--- Modules/Core/Core/Sources/DataManager.swift
+++ Modules/Core/Core/Sources/DataManager.swift
@@ -10,11 +10,17 @@ import RCache
 class DataManager {
     private static var _instance: DataManager?
     private static let lock = NSLock()
-    
-    var isFirstLaunch: Bool = false
-    
+
+    var isFirstLaunch: Bool = false {
+        didSet {
+            RCache.common.save(bool: isFirstLaunch, key: .isFirstLaunch)
+        }
+    }
+
+    var ingredients: [Ingredient] = []
+
     private init(){}
-    
+
     static var instance: DataManager {
         if _instance == nil {
             lock.lock()
@@ -27,9 +33,39 @@ class DataManager {
         }
         return _instance!
     }
-    
+
     func initialize(completion: () -> Void) {
         isFirstLaunch = RCache.common.readBool(key: .isFirstLaunch) ?? false
+        ingredients = (try? RCache.common.read(type: Array<Ingredient>.self, key: .ingredients)) ?? []
         completion()
+    }
+
+    func gacha() -> [Ingredient] {
+        let ingredientPool = Ingredient.allCases
+
+        guard !ingredientPool.isEmpty else { return [] }
+
+        func randomIngredient() -> Ingredient {
+            let roll = Double.random(in: 0...1)
+            var cumulativeRate = 0.0
+
+            for tier in Tier.allCases.sorted(by: { $0.rate > $1.rate }) {
+                cumulativeRate += tier.rate
+                if roll < cumulativeRate {
+                    let filteredIngredients = ingredientPool.filter { $0.tier == tier }
+
+                    if let ingredient = filteredIngredients.randomElement() {
+                        return ingredient
+                    }
+                }
+            }
+
+            return ingredientPool.randomElement()!
+        }
+
+        let result = (0..<5).map { _ in randomIngredient() }
+        ingredients.append(contentsOf: result)
+        try? RCache.common.save(value: ingredients, key: .ingredients)
+        return result
     }
 }
