28d4863547016a65feafb67226ee86bc03524976
Rahmat Trinanda <rahmat3nanda@gmail.com>
2025-02-15 13:19:40 +0700

    add count & from

--- Modules/Core/Core/Model/Card.swift
+++ Modules/Core/Core/Model/Card.swift
@@ -14,18 +14,18 @@ class Card {
     let name: String
     var count: Int
     
-    init(tier: Tier, image: UIImage?, name: String) {
+    init(tier: Tier, image: UIImage?, name: String, count: Int = 1) {
         self.tier = tier
         self.image = image
         self.name = name
-        count = 1
+        self.count = count
     }
     
-    static func from(_ ingredient: Ingredient) -> Card {
-        return Card(tier: ingredient.tier, image: UIImage(named: ingredient.image), name: ingredient.name)
+    static func from(_ ingredient: Ingredient, count: Int = 1) -> Card {
+        return Card(tier: ingredient.tier, image: UIImage(named: ingredient.image), name: ingredient.name, count: count)
     }
     
-    static func from(_ recipe: Recipe) -> Card {
-        return Card(tier: recipe.tier, image: UIImage(named: recipe.icon), name: recipe.name)
+    static func from(_ recipe: Recipe, count: Int = 1) -> Card {
+        return Card(tier: recipe.tier, image: UIImage(named: recipe.icon), name: recipe.name, count: count)
     }
 }
